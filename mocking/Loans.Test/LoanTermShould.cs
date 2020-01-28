c36d3ae81c19443f2efce04b44dbbce3b1a1b3b7
Sebastian <sebastianblari@gmail.com>
2020-01-27 19:43:15 -0600

    Mocking and NUnit testing

---
+++ mocking/Loans.Test/LoanTermShould.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Loans.Domain;
+using Loans.Domain.Applications;
+using NUnit.Framework;
+
+namespace Loans.Test
+{
+    [TestFixture]
+    public class LoanTermShould
+    {
+        [Test]
+        public void ReturnTermInMonths()
+        {
+            var sut = new LoanTerm(1);// sut = system under test
+
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+        }
+
+        [Test]
+        public void NotAllowZeroYears()
+        {
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+        [Test]
+        [Ignore("Need to complete update work")]
+        public void StoreYears()
+        {
+            var sut = new LoanTerm(1);// sut = system under test
+            Assert.That(sut.Years, Is.EqualTo(1));
+        }
+    }
+}
---
+++ mocking/Loans.Test/ProductComparerShould.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Loans.Domain;
+using Loans.Domain.Applications;
+using NUnit.Framework;
+
+namespace Loans.Test
+{
+    [TestFixture]
+    public class ProductComparerShould
+    {
+        private List<LoanProduct> products;
+        private ProductComparer sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+
+        }
+
+        [SetUp]
+        public void Setup() //Runs before every test
+        {
+            products = new List<LoanProduct>()
+            {
+                new LoanProduct(1,"a",1),
+                new LoanProduct(2,"b",2),
+                new LoanProduct(3,"c",3)
+            };
+            sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+        }
+        [TearDown]
+        public void TearDown() //Runs after every test
+        {
+            //Console.WriteLine("Running Tear Down");
+        }
+
+        [Test]
+        [Category("Product Comparison")]
+        public void ReturnCorrectNumberOfComparisons()
+        {
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Has.Exactly(3).Items);
+        }
+
+        [Test]
+        public void NotReturnDuplicateComparisons()
+        {
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Is.Unique);
+        }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct()
+        {
+
+            var expectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Does.Contain(expectedProduct));
+        }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct_WithPartialKnownExpectedValues()
+        {
+
+
+            
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+
+
+            Assert.That(comparisons, Has.Exactly(1)
+                                        .Matches<MonthlyRepaymentComparison>(i => i.ProductName == "a")
+                                        .And
+                                        .Matches<MonthlyRepaymentComparison>(i => i.InterestRate == 1)
+                                        .And
+                                        .Matches<MonthlyRepaymentComparison>(i => i.MonthlyRepayment > 0));
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+
+        }
+    }
+}
