cd8644a9a52679fd318d3d305f45c43dc4f4a90e
Sebastian <sebastianblari@gmail.com>
2020-02-10 16:59:11 -0600

    PizzaDecorator Added
    PizzaDecorator Added

---
+++ PizzaDecorator/PizzaDecorator/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaDecorator
+{
+    public abstract class Pizza
+    {
+        public string description = "Unknown Pizza";
+        public string getDescription()
+        {
+            return description;
+        }
+
+        public abstract int getCost();
+    }
+}
